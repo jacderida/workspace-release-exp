1a9e9dff6d52c4293db296c6d5338971dc4b250f
Chris O'Neil <chriso83@protonmail.com>
2021-10-19 00:44:28 +0100

    feat: provide add three function
    New function to add 3 to a given integer.

--- jacderida-exp-add-lib/src/lib.rs
+++ jacderida-exp-add-lib/src/lib.rs
@@ -6,9 +6,14 @@ pub fn add_two(x: i32) -> i32 {
     x + 2
 }
 
+pub fn add_three(x: i32) -> i32 {
+    x + 3
+}
+
 #[cfg(test)]
 mod tests {
     use super::add_one;
+    use super::add_three;
     use super::add_two;
 
     #[test]
@@ -19,5 +24,10 @@ mod tests {
     #[test]
     fn should_add_two() {
         assert_eq!(3, add_two(1));
+    }
+
+    #[test]
+    fn should_add_three() {
+        assert_eq!(4, add_three(1));
     }
 }
