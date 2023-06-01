4d6744fdd58e9b0bfe324988042a932114b1ce9f
Chris O'Neil <chriso83@protonmail.com>
2023-06-01 15:12:23 +0100

    feat: provide new binary

---
+++ jacderida-exp2-adder2/src/main.rs
@@ -0,0 +1,14 @@
+use jacderida_exp2_add_lib::{add_five, add_four, add_one, add_three, add_two, add_eight, add_nine, add_ten};
+
+fn main() {
+    let x = 100;
+    println!("This is the second adder binary");
+    println!("Use add_one: {}", add_one(x));
+    println!("Use add_two: {}", add_two(x));
+    println!("Use add_three: {}", add_three(x));
+    println!("Use add_four: {}", add_four(x));
+    println!("Use add_five: {}", add_five(x));
+    println!("Use add_eight: {}", add_eight(x));
+    println!("Use add_nine: {}", add_nine(x));
+    println!("Use add_ten: {}", add_ten(x));
+}
